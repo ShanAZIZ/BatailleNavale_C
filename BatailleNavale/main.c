888f35aaf5b57b61116ab6fa7b32544dae21363b
Shan_AZIZ <shan.azizh@gmail.com>
2021-03-18 13:46:38 +0100

    Initial Commit

---
+++ BatailleNavale/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BatailleNavale
+//
+//  Created by Shan Abasse on 18/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
