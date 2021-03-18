5c481b05f69f98b565d525059333b21c936f9e31
Shan_AZIZ <shan.azizh@gmail.com>
2021-03-18 13:56:54 +0100

    Initial - Creation du menu principal dans le main

--- BatailleNavale/main.c
+++ BatailleNavale/main.c
@@ -8,7 +8,38 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    // Initialiser les variables du jeu
+    int choix = 1;
+    int nbTours = 1;
+    int tbJoueur1[8][8];
+    int tbJoueur2[8][8];
+    
+    // Creer les tableaux pour les deux joueurs
+    
+    // Menu principal
+    do {
+        printf("<<<<<<            BATAILLE NAVALE             >>>>>>");
+        printf("\n1. Commencer / Continuer ");
+        printf("\n2. Charger une partie"); //Ne fait rien
+        printf("\n3. Sauvegarder la partie"); //Ne fait rien
+        printf("\n0. Quitter");
+        printf("\nVotre choix : ");
+        scanf("%d",&choix );
+        printf("\n");
+        switch (choix) {
+            case 1:
+                <#statements#>
+                break;
+                
+            default:
+                printf("Choix non actif, ou invalide");
+                break;
+        }
+    }while (choix != 0);
+    
     return 0;
 }
+
+
+
